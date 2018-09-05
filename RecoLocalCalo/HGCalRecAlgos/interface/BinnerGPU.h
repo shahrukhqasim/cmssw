8cf59a4d4f34e6f8b3165c95e23d8d12377b29f3
Nadir Daci <daci.nadir@gmail.com>
2018-09-05 13:46:56 +0200

    Move the definition of ETA_BINS, PHI_BINS, MAX_DEPTH into the header file of BinnerGPU.

--- RecoLocalCalo/HGCalRecAlgos/interface/BinnerGPU.h
+++ RecoLocalCalo/HGCalRecAlgos/interface/BinnerGPU.h
@@ -6,7 +6,15 @@
 #ifndef Binner_GPU_h
 #define Binner_GPU_h
 
+// eta_width = 0.05
+// phi_width = 0.05
+// 2*pi/0.05 = 125
+// 1.4/0.05 = 28
+// 20 (as heuristic)
 
+#define ETA_BINS 28
+#define PHI_BINS 125
+#define MAX_DEPTH 20
 
 struct RechitForBinning {
         long index=-1;
--- RecoLocalCalo/HGCalRecAlgos/src/BinnerGPU.cu
+++ RecoLocalCalo/HGCalRecAlgos/src/BinnerGPU.cu
@@ -1,16 +1,5 @@
 #include "RecoLocalCalo/HGCalRecAlgos/interface/BinnerGPU.h"
 
-// eta_width = 0.05
-// phi_width = 0.05
-// 2*pi/0.05 = 125
-// 1.4/0.05 = 28
-// 20 (as heuristic)
-
-#define ETA_BINS 28
-#define PHI_BINS 125
-#define MAX_DEPTH 20
-
-
 std::shared_ptr<long> computeBins(std::vector<RechitForBinning> layerData) {
     std::shared_ptr<long> hostData(new long[ETA_BINS*PHI_BINS*MAX_DEPTH]);
 
