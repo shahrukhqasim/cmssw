6bf04ec7eef20e7f9c1089b1f0a0fc2c69dbb418
tonydp03 <tony.dipilato03@gmail.com>
2018-11-08 16:53:17 +0100

    Integrate Histogram Class into the Kernel

--- RecoLocalCalo/HGCalRecAlgos/src/BinnerGPU.cu
+++ RecoLocalCalo/HGCalRecAlgos/src/BinnerGPU.cu
@@ -2,14 +2,13 @@
 #include "HeterogeneousCore/CUDAUtilities/interface/GPUVecArray.h"
 #include <math.h>
 
+#include "RecoLocalCalo/HGCalRecAlgos/interface/GPUHist2D.h"
 
 
 namespace BinnerGPU {
 
-typedef GPU::VecArray<int,MAX_DEPTH> RequiredBin;
 
-
-__global__ void kernel_compute_histogram(RechitForBinning*dInputData, RequiredBin*dOutputData, const size_t numRechits) {
+  __global__ void kernel_compute_histogram(RechitForBinning*dInputData, histogram2D<int,ETA_BINS, PHI_BINS, MAX_DEPTH> *dOutputData, const size_t numRechits) {
 
     size_t rechitLocation = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -19,33 +18,36 @@ __global__ void kernel_compute_histogram(RechitForBinning*dInputData, RequiredBi
     float eta = dInputData[rechitLocation].eta;
     float phi = dInputData[rechitLocation].phi;
     unsigned int index = dInputData[rechitLocation].index;
+   
+    dOutputData->fillBinGPU(eta, phi, index);
 
-    int etaIndex = floor((abs(eta) - 1.6) / 0.05);
-    int phiIndex = floor((phi + M_PI) / 0.05);
-
-    dOutputData[phiIndex*ETA_BINS + etaIndex].push_back(index);
-}
+  }
 
 
+  float minEta = 1.6;
+  float maxEta = 3.0;
+  float minPhi = -M_PI;
+  float maxPhi = M_PI;
 
-std::shared_ptr<int> computeBins(std::vector<RechitForBinning> layerData) {
-    std::shared_ptr<int> hOutputData(new int[ETA_BINS*PHI_BINS*MAX_DEPTH]);
+  std::shared_ptr<int> computeBins(std::vector<RechitForBinning> layerData) {
+    histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> hOutputData(minEta, maxEta, minPhi, maxPhi);
 
     // Allocate memory and put data into device
-    RequiredBin* dOutputData;
+    histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH> *dOutputData;
     RechitForBinning* dInputData;
-    cudaMalloc(&dOutputData, sizeof(RequiredBin)*ETA_BINS*PHI_BINS);
+    cudaMalloc(&dOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>));
     cudaMalloc(&dInputData, sizeof(RechitForBinning)*layerData.size());
     cudaMemcpy(dInputData, layerData.data(), sizeof(RechitForBinning)*layerData.size(), cudaMemcpyHostToDevice);
-    cudaMemset(dOutputData, 0x00, sizeof(RequiredBin)*ETA_BINS*PHI_BINS);
-
+    cudaMemset(dOutputData, 0x00, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>));
+    cudaMemcpy(dOutputData, &hOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>), cudaMemcpyHostToDevice);
+  
     // Call the kernel
     const dim3 blockSize(1024,1,1);
     const dim3 gridSize(ceil(layerData.size()/1024.0),1,1);
     kernel_compute_histogram <<<gridSize,blockSize>>>(dInputData, dOutputData, layerData.size());
 
     // Copy result back!
-    cudaMemcpy(dOutputData, hOutputData.get(), sizeof(int)*ETA_BINS*PHI_BINS*MAX_DEPTH, cudaMemcpyDeviceToHost);
+    cudaMemcpy(dOutputData, &hOutputData, sizeof(histogram2D<int, ETA_BINS, PHI_BINS, MAX_DEPTH>), cudaMemcpyDeviceToHost);
 
     // Free all the memory
     cudaFree(dOutputData);
@@ -53,6 +55,6 @@ std::shared_ptr<int> computeBins(std::vector<RechitForBinning> layerData) {
 
     
     return hOutputData;
-}
+  }
 
 }
